70b3c50602d197303747aef826db740ee988c27f
Ryel Banfield <ryelbanfield@gmail.com>
2021-05-09 08:52:01 -0400

    Push completed project

---
+++ README.md
@@ -0,0 +1,41 @@
+# Giphy Search built with JavaScript & Giphy API
+
+> A simple website that returns you gifs from Giphy website.
+
+Additional description about the project and its features.
+
+## Built With
+
+- JavaScript
+
+## Live Demo
+
+[Live Demo Link](https://livedemo.com)
+
+## Authors
+
+👤 **Ryel Banfield**
+
+- GitHub: [@RyelBanfield](https://github.com/ryelbanfield)
+- Twitter: [@RyelBanfield](https://twitter.com/ryelbanfield)
+- LinkedIn: [Ryel Banfield](https://www.linkedin.com/in/ryel-banfield/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](LICENSE) licensed.
---
+++ javascript/main.js
@@ -0,0 +1,50 @@
+document.querySelector(".js-go").addEventListener("click", function () {
+  var input = document.querySelector("input").value;
+  var url =
+    "http://api.giphy.com/v1/gifs/search?q=" +
+    input +
+    "&api_key=U9aIyKli7Jxp38TqdOC2yePIEm07ZSHb";
+
+  var GiphyAJAXCall = new XMLHttpRequest();
+  GiphyAJAXCall.open("GET", url);
+  GiphyAJAXCall.send();
+
+  GiphyAJAXCall.addEventListener("load", function (callResponse) {
+    var data = callResponse.target.response;
+    pushToDOM(data);
+  });
+});
+
+document
+  .querySelector(".js-userinput")
+  .addEventListener("keyup", function (keyPressed) {
+    if (keyPressed.which == 13) {
+      var input = document.querySelector("input").value;
+      var url =
+        "http://api.giphy.com/v1/gifs/search?q=" +
+        input +
+        "&api_key=U9aIyKli7Jxp38TqdOC2yePIEm07ZSHb";
+
+      var GiphyAJAXCall = new XMLHttpRequest();
+      GiphyAJAXCall.open("GET", url);
+      GiphyAJAXCall.send();
+
+      GiphyAJAXCall.addEventListener("load", function (callResponse) {
+        var data = callResponse.target.response;
+        pushToDOM(data);
+      });
+    }
+  });
+
+function pushToDOM(input) {
+  var response = JSON.parse(input);
+  var imageUrls = response.data;
+  var container = document.querySelector(".js-container");
+
+  container.innerHTML = "";
+
+  imageUrls.forEach(function (image) {
+    var imageUrl = image.images.fixed_height.url;
+    container.innerHTML += '<img src="' + imageUrl + '" class="m-1">';
+  });
+}
